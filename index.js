7721edd5530e07f80f41d81973ee70aad0df8b2f
dayee0318 <dy0318m@gmail.com>
2023-04-07 23:29:34 -0600

    query and param

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-var figlet = require('figlet');
+const express = require('express')
+const app = express()
+const port = 3000
 
-    figlet('Hello World!!!!', function(err, data) {
-        if (err) {
-            console.log('Something went wrong...');
-            console.dir(err);
-            return;
-        }
-        console.log(data)
-    });
+app.get('/', (req, res) => { // root
+    res.send('Hello World!')
+})
+
+app.get('/dog', (req, res) => { // routing
+    res.json({'sound' : 'Woof'}) // API created
+})
+
+app.get('/cat', (req, res) => { // routing
+    res.json({'sound' : 'Meow'}) // json(Java Script Object Notation)
+})
+
+app.get('/user/:id', (req, res) => {    
+    const p = req.params;    // Youtube users +++... cannot create every time -> use parameters to create each user's
+    console.log(p.id)
+
+    const q = req.query;        // Query [Key:value]
+    console.log(q)
+    console.log(q.name)
+
+    res.send({'userId': q.name});
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
